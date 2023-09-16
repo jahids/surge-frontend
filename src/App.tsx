dd33f662f7311bc3fd5bf6ccc5091d5cea29bac3
jahids <evanjahid321@gmail.com>
2023-09-16 16:30:15 +0600

    branch updated

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,55 @@
 
 // export default App;
 
+// import React from 'react';
+// import { useCookies } from 'react-cookie';
+// import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+// import Navbar from './Components/Navbar';
+// import Contact from './Pages/Contact';
+// import About from './Pages/About';
+// import Home from './Pages/Home';
+// import LoginPage from './Pages/Login/LoginPage';
+// import PrivatePage from './Pages/private/PrivatePage';
+// import LoginForm from './Components/Navbar/LoginForm';
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import datapage from './Pages/private/Datapage';
+// import Datapage from './Pages/private/Datapage';
+
+// const App: React.FC = () => {
+//   const [cookies] = useCookies(['mytoken']);
+//   const isAuthenticated = !!cookies.mytoken;
+
+//   console.log('cookies', cookies.mytoken);
+
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route
+//           path="/protected"
+//           element={isAuthenticated ? <PrivatePage /> : <Navigate to="/login" />}
+//         />
+
+//         <Route
+//           path="/data"
+//           element={isAuthenticated ? <Datapage /> : <Navigate to="/login" />}
+//         />
+
+//         <Route path="/login" element={<LoginPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+// updated codebase
+
 import React from 'react';
 import { useCookies } from 'react-cookie';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
@@ -30,37 +79,35 @@ import Contact from './Pages/Contact';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import LoginPage from './Pages/Login/LoginPage';
-import PrivatePage from './Pages/private/PrivatePage';
-import LoginForm from './Components/Navbar/LoginForm';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import datapage from './Pages/private/Datapage';
+// import PrivateRoute from './Components/PrivateRoute';
 import Datapage from './Pages/private/Datapage';
+import PrivateRoute from './Hooks/PrivateRoute';
 
 const App: React.FC = () => {
   const [cookies] = useCookies(['mytoken']);
   const isAuthenticated = !!cookies.mytoken;
 
-  console.log('cookies', cookies.mytoken);
-
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/protected"
-          element={isAuthenticated ? <PrivatePage /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/data"
-          element={isAuthenticated ? <Datapage /> : <Navigate to="/login" />}
-        />
-
         <Route path="/login" element={<LoginPage />} />
+        {/* Use PrivateRoute for protected routes */}
+        {/* <PrivateRoute
+          path="/protected"
+          element={<PrivatePage />}
+          isAuthenticated={isAuthenticated}
+        /> */}
+        {/* <PrivateRoute
+          path="/data"
+          element={<Datapage />}
+          isAuthenticated={isAuthenticated}
+        /> */}
+        {/* Handle unknown routes */}
+        <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/AllRoute.tsx
@@ -0,0 +1,29 @@
+// const allRoute = [
+//   {
+//     path: '/',
+//     component: MainDash,
+//     roles: ['admin', 'user'],
+//   },
+//   {
+//     path: '/entryform',
+//     component: EntryForm,
+//     roles: ['admin', 'user'],
+//   },
+//   {
+//     path: '/profile',
+//     component: ProfilePage,
+//     roles: ['admin', 'user'],
+//   },
+
+//   {
+//     path: '/advchart',
+//     component: PredectionChart,
+//     roles: ['admin', 'user'],
+//   },
+//   {
+//     path: '/right',
+//     component: RightSide,
+//     roles: ['admin', 'user'],
+//   },
+//   // Add more admin routes as needed
+// ];
