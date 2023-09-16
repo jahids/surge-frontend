f5b1619c34c988306fa340f70d6be48707d34090
jahids <evanjahid321@gmail.com>
2023-09-16 13:57:16 +0600

    axios instance done

---
+++ src/lib/AxiosInstance.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+
+const AxiosInstance = () => {
+  const [cookies] = useCookies(['mytoken']);
+
+  // Create an Axios instance
+  const instance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 10000, // Adjust the timeout as needed
+    withCredentials: true,
+    headers: {
+      'X-Custom-Header': 'foobar',
+      Authorization: `Bearer ${cookies.mytoken || ''}`,
+    },
+  });
+
+  return instance;
+};
+
+export default AxiosInstance;
