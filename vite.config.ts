87e3515619c78a4d9dd264650f675553fe112fb5
fazlulShanto <fazlulcse17@gmail.com>
2023-10-03 15:17:45 +0600

    added loader

--- vite.config.ts
+++ vite.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://192.168.68.56:5678', // Your backend server URL
-        //target: 'http://localhost:4500',
+        // target: 'http://localhost:4500',
         changeOrigin: true,
         // You can add other proxy options here if needed
         // For example, to rewrite the path:
